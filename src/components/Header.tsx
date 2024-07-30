f491917288115a09253cba51a35d8eae1b1edfba
CuongVnZ <eldrienguyen@gmail.com>
2024-07-30 23:42:05 +1000

    Chore: (Cuong) fixed some stuffs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -106,23 +106,3 @@ export default function Component() {
     </header>
   );
 }
-
-function DiscIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="10" />
-      <circle cx="12" cy="12" r="2" />
-    </svg>
-  );
-}
